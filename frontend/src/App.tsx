b06b75f5f5538f1759af4a0c5d9754925573a080
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-20 19:42:19 +0100

    route bug fixing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import './App.css';
 import Signup from './components/Signup';
 import Landing from './components/Landing';
-import { Routes, Route, BrowserRouter, useNavigate } from 'react-router-dom';
+import { Routes, Route, Router, useNavigate } from 'react-router-dom';
 
-const App =() => {
+const App = () => {
 
   const navigate = useNavigate();
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Landing navigate={navigate} />} />
         <Route path="/signup" element={<Signup navigate={navigate} />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 

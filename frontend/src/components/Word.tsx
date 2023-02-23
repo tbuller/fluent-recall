9c038f1023fd2e404dbd9e613419164c941db510
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-23 18:16:49 +0100

    each word displayed as a card

--- frontend/src/components/Word.tsx
+++ frontend/src/components/Word.tsx
@@ -1,16 +1,20 @@
+import "./styling/Word.css";
 import React from 'react';
 import { useState, useEffect } from 'react';
 
 const Word = ({ words }: { words: any }) => {
 
-  const showWords = () => {
-    console.log(words);
-  }
+  // const showWords = () => {
+  //   console.log(words);
+  // }
 
   return (
     <div className="word-container">
-    <div className="word-container">Hello world</div>
-    <button onClick={showWords}>click</button>
+    <div>
+      {words.map((w: any) => 
+        <span className="word-card" key={w._id}>{w.english}</span>
+        )}
+    </div>  
     </div>
   )
 }

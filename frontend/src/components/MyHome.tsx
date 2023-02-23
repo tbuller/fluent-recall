f963c878ed76509df176dcb52e02c342a6cf6272
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-23 10:33:58 +0100

    tailwindcss set up

--- frontend/src/components/MyHome.tsx
+++ frontend/src/components/MyHome.tsx
@@ -1,9 +1,13 @@
+import { GiItalia, GiFrance, GiSpain } from 'react-icons/gi';
 import React from 'react';
 
 const MyHome = ({ navigate }: {navigate: any}) => {
   return (
     <div>
-      This is your home page
+    <h1>This is your home page</h1>
+    <button><GiItalia /></button>
+    <button><GiFrance /></button>
+    <button><GiSpain /></button>
     </div>
   )
 }

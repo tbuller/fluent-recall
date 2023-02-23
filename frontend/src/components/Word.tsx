c9e33cde2fe14f8c3cb2628c18c2493d0cb50580
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-23 19:16:53 +0100

    button styling done and delete fetch set up

--- frontend/src/components/Word.tsx
+++ frontend/src/components/Word.tsx
@@ -4,15 +4,29 @@ import { useState, useEffect } from 'react';
 
 const Word = ({ words }: { words: any }) => {
 
-  // const showWords = () => {
-  //   console.log(words);
-  // }
+  const [deletedId, setDeletedId] = useState("")
+
+  const deleteWord = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setDeletedId(event.currentTarget.value)
+    fetch("http://localhost/8080/words", {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: deletedId
+    })
+  }
 
   return (
     <div className="word-container">
     <div>
       {words.map((w: any) => 
-        <span className="word-card" key={w._id}>{w.english}</span>
+        <span className="word-card" key={w._id}>
+          {w.english}
+        <span className="delete-card-container">
+        <button className="delete-card" value={w._id} onClick={deleteWord}>X</button>
+        </span>  
+        </span>
         )}
     </div>  
     </div>

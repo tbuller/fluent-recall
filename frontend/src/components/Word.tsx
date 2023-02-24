beb86f8f3ba57bddd5c69203eea6c0117f455719
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-24 20:22:55 +0100

    conditionally changing the colours of the cards

--- frontend/src/components/Word.tsx
+++ frontend/src/components/Word.tsx
@@ -23,15 +23,41 @@ const Word = ({ words, handleWordDelete, tab }: { words: any, handleWordDelete: 
   return (
     <div className="word-container">
     <div>
-      {words.map((w: any) =>
-      w.language === tab && 
-        <span className="word-card" key={w._id}>
+    {words.map((w: any) => {
+    const previousTries = w.past10;
+    let backgroundColor = "";
+    
+    const numPasses = previousTries.filter((tryResult: any) => tryResult === "Pass").length;
+    const totalTries = previousTries.length;
+
+    if (totalTries < 5) {
+      backgroundColor = "grey";
+    } else {
+      const passRate = numPasses / totalTries;
+
+      if (passRate >= 0.7) {
+        backgroundColor = "green";
+      } else if (passRate >= 0.5 && passRate < 0.7) {
+        backgroundColor = "amber";
+      } else {
+        backgroundColor = "red";
+      }
+    }
+    if (w.language === tab) {
+      return (
+        <span className="word-card" key={w._id} style={{ backgroundColor }}>
           {w.english}
         <span className="delete-card-container">
-        <button className="delete-card" value={w._id} onClick={deleteWord}>X</button>
-        </span>  
+        <button className="delete-card" value={w._id} onClick={deleteWord}>
+          X
+        </button>
         </span>
-        )}
+        </span>
+      );
+    } else {
+      return null;
+    }
+  })}
     </div>  
     </div>
   )

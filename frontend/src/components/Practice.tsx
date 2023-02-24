f3a184c0525051c498645ba7636c42b3beb85f08
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-24 19:35:34 +0100

    slider component implemented

--- frontend/src/components/Practice.tsx
+++ frontend/src/components/Practice.tsx
@@ -3,6 +3,9 @@ import { IoMdSchool } from 'react-icons/io';
 import { VscRunAll } from 'react-icons/vsc';
 import React from 'react';
 import { useState, useEffect } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Practice = ({ words, tab }: { words: any, tab: string }) => {
 
@@ -26,9 +29,10 @@ const Practice = ({ words, tab }: { words: any, tab: string }) => {
   }
 
   return (
-    <div>
+    <div className="practice-card-container">
+      <Slider dots={true} infinite={true} speed={500} slidesToShow={3} slidesToScroll={1} className="center">
       {words.map((w: any) => 
-        w.language === tab &&
+        w.language === tab &&        
         <div className="practice-card" key={w._id}>
         <IoMdSchool className="school-icon" />
         <label className="practice-label">Your word in english is:</label>
@@ -38,9 +42,10 @@ const Practice = ({ words, tab }: { words: any, tab: string }) => {
         <button value={`${w._id}:${w.target}`} onClick={compareAnswer} className="submit-answer">
         <VscRunAll />
         </button>
-        <div>More information about this card</div>
+        <div>More information about this card</div>        
         </div>
     )}
+    </Slider>
     </div>
   )
 }

44496ac846f600d20705cf4b52f13330c301ba0c
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-28 12:32:50 +0100

    icon added, percentage calculation and more styling

--- frontend/src/components/InfoCard.tsx
+++ frontend/src/components/InfoCard.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { useEffect } from 'react'
+import { RxSpeakerLoud } from 'react-icons/rx';
 
 const InfoCard = (w : any) => {
 
-  const talk = async () => {
+  const talk = () => {
     const utterance = new SpeechSynthesisUtterance(w.w.target);
     utterance.rate = 1;
-    utterance.lang = 'fr'
+    if (w.w.language === "French") {
+      utterance.lang = "fr"
+    } else if (w.w.language === "Spanish") {
+      utterance.lang = "es"
+    } else {
+      utterance.lang = "it"
+    }
     speechSynthesis.speak(utterance);
   }
 
   return (
     <div className="info-card-container">
-    <div className="phnetic-info"></div>
+    <span className="answer-info-container">
+    <div className="answer-info">{w.w.target}</div>
+    <button onClick={talk} className="talk-button"><RxSpeakerLoud /></button>
+    </span>
     <div className="date-info">{`- You added this word on ${new Date(w.w.createdAt).toDateString()}`}</div>
-    <div onClick={talk} className="success-info">{`- Success:${(w.w.past10).filter((x: any) => x === "Pass").length}/${(w.w.past10).length} correct attempts`}</div>
+    <div className="success-info">{`- Success: ${(w.w.past10).filter((x: any) => x === "Pass").length}/${(w.w.past10).length} correct attempts (${Math.round(((w.w.past10).filter((x: any) => x === "Pass").length / (w.w.past10).length) * 100)}%)`}</div>
     </div>
   )
 }

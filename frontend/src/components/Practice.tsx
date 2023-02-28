ed5444453310307c1c5f419bee75a3bac4b4309c
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-02-28 13:53:59 +0100

    cards background color changed conditionally on the success rate

--- frontend/src/components/Practice.tsx
+++ frontend/src/components/Practice.tsx
@@ -43,12 +43,32 @@ const Practice = ({ words, tab }: { words: any, tab: string }) => {
   return (
     <div className={`practice-card-container practice-card-container--${tab}`}>
       <Slider dots={false} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1} className="center" ref={sliderRef}>
-      {words.map((w: any) => 
-        w.language === tab &&
-        <div className="practice-card" key={w._id}>
+      {words.map((w: any) => {
+        const previousTries = w.past10;
+        let backgroundColor = "background-color:grey;";
+        
+        const numPasses = previousTries.filter((tryResult: any) => tryResult === "Pass").length;
+        const totalTries = previousTries.length;
+    
+        if (totalTries < 5) {
+          backgroundColor = "grey";
+        } else {
+          const passRate = numPasses / totalTries;
+    
+          if (passRate >= 0.7) {
+            backgroundColor = "green";
+          } else if (passRate >= 0.5 && passRate < 0.7) {
+            backgroundColor = "amber";
+          } else {
+            backgroundColor = "red";
+          }
+        }
+        if (w.language === tab) { 
+        return (  
+        <div className={`practice-card practice-card-${backgroundColor}`} key={w._id}>
         <IoMdSchool className="school-icon" />
-        <label className="practice-label">Your word in english is:</label>
-        <div className="practice-word">{w.english}</div>
+        {/* <label className="practice-label">Your word in english is:</label> */}
+        <p className="practice-word">{w.english}</p>
         <label className="practice-prompt">{`So in ${w.language} it would be:`}</label>        
         <input type="text" onChange={(event) => setAnswer(event.target.value)} className="input-answer" />
         <button value={`${w._id}:${w.target}`} onClick={compareAnswer} className="submit-answer">
@@ -63,10 +83,13 @@ const Practice = ({ words, tab }: { words: any, tab: string }) => {
         {result === "unattempted" ? <span className="rectangle-unattempted">[]</span> : <span className={`rectangle-${result}`} key={Math.random()}>[]</span>}
         </span>
         <div className={`practice-result-${result}`}>{result === "Pass" ? <TiTick /> : <ImCross />}{w.target}</div>
-        <div onClick={() => setShowInfo(!showInfo)}>{showInfo ? <div className="more-info-container"><AiFillMinusCircle className="more-info-button" /> <div className="more-info-text">Word Info (includes spoilers)</div></div> : <div className="more-info-container"><AiFillPlusCircle className="more-info-button" /> <div className="more-info-text">Word Info</div></div>}</div> 
+        <div onClick={() => setShowInfo(!showInfo)}>{showInfo ? <div className="more-info-container"><AiFillMinusCircle className="more-info-button" /> <div className="more-info-text">Word Info (includes spoilers)</div></div> : <div className="more-info-container"><AiFillPlusCircle className="more-info-button" /> <div className="more-info-text">Word Info (includes spoilers)</div></div>}</div> 
         {showInfo && <InfoCard w={w} />}       
         </div>
     )}
+    else {
+      return null;
+    }})}
     </Slider>
     </div>
   )

5aeed46807e24ed498a05dd301ef039a61ac1199
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-03-01 09:40:25 +0100

    landing page expanded and styling added

--- frontend/src/components/Landing.tsx
+++ frontend/src/components/Landing.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import "./styling/Landing.css"
 
 const Landing = ({ navigate }: {navigate: any}) => {
+
+  const navigateLogin = () => {
+    navigate('/login');
+  }
+
+  const navigateSignup = () => {
+    navigate('/signup');
+  }
+
   return (
-    <div>Welcome to Fluent Recall!</div>
+    <div className="landing-container">
+    <h1 className="landing-greeting">Welcome to Fluent Recall!</h1>
+    <h3 className="landing-prompt">Create an account or log in if you are already registerd with us</h3>
+    <button className="nav-button" onClick={navigateLogin}>Log in</button>
+    <button className="nav-button" onClick={navigateSignup}>Sign up</button>
+    </div>
   )
 }
 
